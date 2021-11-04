b3e5ebda41d104b4af796482c1cb62ebdf1e8b6e
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2021-11-04 14:37:35 +0100

    Update ModeratorController.java

--- src/main/java/com/example/demo/controller/ModeratorController.java
+++ src/main/java/com/example/demo/controller/ModeratorController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.QAMessage;
 import com.example.demo.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @CrossOrigin("*") //makes it so that everyone can access the api Alternative use: origins = {"http://localhost:3000/"}
@@ -15,4 +19,10 @@ public class ModeratorController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    //Check if message is unapproved.
+    @GetMapping
+    public List<QAMessage> getUnapprovedMessages(){
+        return messageRepository.findUnapprovedMessage();
+    }
 }
