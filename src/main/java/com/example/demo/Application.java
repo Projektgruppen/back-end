1b1c5cb906bb89f0c65298121333a8e7dcdb2c86
JeBenzon <jonathaneisbenzon@gmail.com>
2021-11-16 13:43:05 +0100

    2 new messages added for testing purpose when doing out loud testing with manova on thursday

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -34,6 +34,20 @@ public class Application implements CommandLineRunner {
         qaMessage2.setApprove(true);
         messageRepository.save(qaMessage2);
 
+        //Test example 2
+        QAMessage qaMessage3 = new QAMessage();
+        qaMessage3.setQuestion("SPAAAAM!!");
+        qaMessage3.setAnswer(null);
+        qaMessage3.setApprove(false);
+        messageRepository.save(qaMessage3);
+
+        //Test example 2
+        QAMessage qaMessage4 = new QAMessage();
+        qaMessage4.setQuestion("SPAAAAM!! 2");
+        qaMessage4.setAnswer(null);
+        qaMessage4.setApprove(false);
+        messageRepository.save(qaMessage4);
+
     }
 
 }
