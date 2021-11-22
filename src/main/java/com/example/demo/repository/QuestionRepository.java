6ae095a592e1a2e1037c05d7e31e0b3c1a0348ac
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2021-11-22 11:56:17 +0100

    Deleted unused code

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -10,17 +10,11 @@ import java.util.Collection;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository <Question, Long> {
-    @Query(value = "SELECT * FROM QUESTION WHERE approve = true", nativeQuery = true)
-    List<Question> getAllApprovedQuestions();
-
 
     @Query(value = "SELECT answers.answer, questions.question\n" +
             "FROM answers\n" +
             "RIGHT JOIN  questions ON answers.id = questions.id\n" +
             "WHERE questions.session_id = ?1 AND questions.approve = true", nativeQuery = true)
     List<Collection> getApprovedQuestions(int session_id);
-
-    @Query("SELECT u FROM Answer u WHERE u.id = ?1")
-    Answer findUserByStatus(long id);
 
 }
